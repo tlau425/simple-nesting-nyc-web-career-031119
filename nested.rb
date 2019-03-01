d7b724ebe270a8120c86931b1a02cd1c4c14de79
Tommy Lau <tlau425@yahoo.com>
2019-03-01 06:01:19 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,6 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
+      programmer_hash[:grace_hopper]
 
 
 end
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     array = programmer_hash[:dennis_ritchie][:languages]
+     array.join
 end
 
 def adding_matz
@@ -79,7 +83,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+ programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+programmer_hash
     
 end
 
@@ -102,6 +107,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
+     
      
 end
 
@@ -122,4 +130,6 @@ def adding_to_dennis
         }
      }
 
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
 end
